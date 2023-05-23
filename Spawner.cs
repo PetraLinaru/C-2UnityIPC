076fc81163263fbba144fab64d171f350dcc5e82
Lînaru Petra <117666069+PetraLinaru@users.noreply.github.com>
2023-05-23 17:16:08 +0300

    add comments, clean code

--- Spawner.cs
+++ Spawner.cs
@@ -1,99 +1,144 @@
-// Include the necessary headers
-#include <iostream>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include<string>
 
-#pragma comment(lib, "ws2_32.lib")
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEditor.PackageManager;
+using UnityEngine;
 
-int main()
+
+public class Spawner : MonoBehaviour
 {
-    // Initialize Winsock
+    public GameObject gameObjectPrefab;
+    Socket SeverSocket = null;
+    Thread Socket_Thread = null;
+    bool Socket_Thread_Flag = false;
 
-    //Structure to hold info about WinSocket
-    WSADATA wsData;
 
-    //Initialize 0x0202 by using makeword(2,2)
-    //Send a pointer to wsData's memory location
-    //WSAStartup initializes the library with the version and the info about Winsocket^^
-    //The WSAStartup function initiates use of the Winsock DLL by a process.
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0)
+   
+    int currentNumber = 0;
+    int sentData = 0;
+    
+    //Create a thread to do work *function is Dowrk, get it ? heehe*
+    //sets the thread flag to true, so that we know while the flag is true, we'll do work
+    //start the thread
+    void Awake()
     {
-        std::cerr << "Failed to initialize Winsock" << std::endl;
-        return 1;
+        Socket_Thread = new Thread(Dowrk);
+        Socket_Thread_Flag = true;
+        Socket_Thread.Start();
     }
 
-    // Create a socket with  s = socket(domain, type, protocol);
-    //AF_INET stands for IPV4, indicates IPV4 domain
-    //SOCK_STREAM provides sequenced, two-way byte streams with a transmission mechanism for stream data , TCP
-    //protocol is unspecified (a value of 0) so the system selects a protocol that supports the requested socket type
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET)
+    private void Dowrk()
     {
-        std::cerr << "Failed to create socket" << std::endl;
-        WSACleanup();
-        return 1;
-    }
+        //Create a socket
+        SeverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-    // Set up the server address
-    // htons(38000) converts the decimal value 38000 to its equivalent in network byte order
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(9999); // Use the same port number as in the Unity side
+        //Pin an endpoint on port 9999 (same as on C++ side)
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9999);
+        //bind that endpoint to the socket
+        SeverSocket.Bind(ipep);
 
-    // inet_pton is used convert the IP address string to binary format
-    if (inet_pton(AF_INET, "127.0.0.1", &(serverAddress.sin_addr)) != 1)
-    {
-        std::cerr << "Failed to convert IP address" << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
+        //Listening for max 10 pending requests, then start accepting
+        SeverSocket.Listen(10);
+       
+        Debug.Log("Socket Standby....");
+        Socket client = SeverSocket.Accept();//client
+        Debug.Log("Socket Connected.");
 
-    // Connect to the server
-    //attempt to establish a connection to the server using the created socket and the server address
-    if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR)
-    {
-        std::cerr << "Failed to connect to the server" << std::endl;
-        closesocket(clientSocket);
-        WSACleanup();
-        return 1;
-    }
+        //create a client end point to recieve info from c++
+        IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+        //create a network stream to recieve data
+        NetworkStream recvStm = new NetworkStream(client);
 
-
-
-    // Send number of objects to be spawn to the server until 'Q' is entered
-    std::string input;
-    while (true)
-    {
-        std::cout << "Enter a number (or 'Q' to quit): ";
-        std::cin >> input;
-
-        if (input == "Q" || input == "q")
+        while (Socket_Thread_Flag)
         {
-            std::cout << "Well that's all folks! Thanks, BYEEE!!!!!";
-            break;
+            byte[] receiveBuffer = new byte[1024 * 80];
+            try
+            {
+
+                //print (recvStm.Read(receiveBuffer, 0, receiveBuffer.Length));
+                //read data
+                //if unavailable data/ no data sent, we wait
+                if (recvStm.Read(receiveBuffer, 0, receiveBuffer.Length) == 0)
+                {
+                    // when disconnected , wait for new connection.
+                    client.Close();
+                    SeverSocket.Close();
+
+                    SeverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    ipep = new IPEndPoint(IPAddress.Any, 10000);
+                    SeverSocket.Bind(ipep);
+                    SeverSocket.Listen(10);
+                    Debug.Log("Socket Standby....");
+                    client = SeverSocket.Accept();//client
+                    Debug.Log("Socket Connected.");
+
+                    clientep = (IPEndPoint)client.RemoteEndPoint;
+                    recvStm = new NetworkStream(client);
+
+                }//else, we read data and update number of objs to be spawned
+                else
+                {
+
+
+                    string Test = Encoding.Default.GetString(receiveBuffer);
+                    sentData += int.Parse(Test);
+                    
+                    print(Test);
+
+                }
+
+
+            }
+
+            catch (Exception e)
+            {
+                Socket_Thread_Flag = false;
+                client.Close();
+                Debug.Log(e);
+                Debug.Log("Closed due to exception!!");
+                SeverSocket.Close();
+                continue;
+            }
+
         }
 
-        int numObjects = std::stoi(input);
-        std::string data = std::to_string(numObjects);
-
-        if (send(clientSocket, data.c_str(), data.size(), 0) == SOCKET_ERROR)
-        {
-            std::cerr << "Failed to send data" << std::endl;
-            closesocket(clientSocket);
-            WSACleanup();
-            return 1;
-        }
-
-        std::cout << "Sent data: " << numObjects << ", " << data.size() << std::endl;
     }
 
-    // Close the socket and clean up Winsock
-    closesocket(clientSocket);
-    WSACleanup();
 
-    return 0;
+    //update is called in each frame
+    //also Instantiate could be used in unity in main thread functions
+    //we keep a currentNumber of objs in the scene so whenever we want more, 
+    //we'll create inside the while loop
+    private void Update()
+    {
+        while(currentNumber<sentData)
+        {
+            Debug.Log("here we gotta instantiate " + (sentData - currentNumber) + " more objs");
+            Vector3 spawnLocation = new Vector3(UnityEngine.Random.Range(-30, 31), 5, UnityEngine.Random.Range(-22, 23));
+            Instantiate(gameObjectPrefab, spawnLocation, Quaternion.identity);
+            currentNumber++;
+        }
+    }
+
+    //closing function
+    void OnApplicationQuit()
+    {
+        try
+        {
+            Socket_Thread_Flag = false;
+            Socket_Thread.Abort();
+            SeverSocket.Close();
+            Debug.Log("Bye~~");
+        }
+
+        catch
+        {
+            Debug.Log("Error when finished...");
+        }
+    }
+
+
 }
-
